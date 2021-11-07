bdbb437ba4380bef8aece638170fdedb8bd43131
Your Name <you@example.com>
2021-11-07 13:15:39 +0600

    added folder

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>This Is An Inventory</h2>
+        </div>
+    );
+};
+
+export default Inventory;
