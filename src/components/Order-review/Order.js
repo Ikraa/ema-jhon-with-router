bebce0e562e68220faea5fd924c71d1d9d5673b6
Your Name <you@example.com>
2021-11-14 02:17:03 +0600

    added navLink

--- src/components/Order-review/Order.js
+++ src/components/Order-review/Order.js
@@ -1,11 +1,14 @@
-import React from 'react';
+// import React from 'react';
+// import useProducts from '../../Hooks/useProducts';
 
-const Order = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+// const Order = () => {
+//     const [products] = useProducts();
+//     return (
+//         <div>
+//             <h1>{products.length}</h1>
+//             <h2>This is Order Review</h2>
+//         </div>
+//     );
+// }
 
-export default Order;
+// export default Order;
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import useCarts from '../../hooks/useCarts';
+import useProducts from '../../hooks/useProducts';
+import { removeFromDb } from '../../utilities/fakedb';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCarts(products);
+
+    const handleRemove = (key) => {
+        const newCart=cart.filter(product=>product.key!==key);
+        removeFromDb(key);
+        setCart(newCart);
+    };
     return (
-        <div>
+        <div className='shop-container'>
+            <div className='product-container'>
+
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}>
+                    </ReviewItem>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
+            <h1>{products.length}</h1>
+            <h3>{cart.length}</h3>
             <h2>This is Order Review</h2>
+
         </div>
     );
 };
